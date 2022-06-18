f305fabae23a2e1472fff2dd72e04046a122afd7
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-18 20:06:59 +0100

    refactor: add a function to call multiple names

--- hello/hello.go
+++ hello/hello.go
@@ -10,9 +10,11 @@ func main() {
   log.SetPrefix("greetings: ")
   log.SetFlags(0)
 
-  message, err := greetings.Hello("")
+  names := []string{"Gladys", "Samantha", "Darrin"}
+
+  messages, err := greetings.Hellos(names)
   if err != nil {
     log.Fatal(err)
   }
-  fmt.Println(message)
+  fmt.Println(messages)
 }
