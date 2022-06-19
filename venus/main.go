7ede0c783ebe8ef2db4d44ed5727679f08e5191c
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-19 16:01:13 +0100

    feat: add post and get by id endpoints

--- venus/main.go
+++ venus/main.go
@@ -15,6 +15,8 @@ type album struct {
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
@@ -29,6 +31,26 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func postAlbums(c *gin.Context) {
+	var newAlbum album
 
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
 
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
 
